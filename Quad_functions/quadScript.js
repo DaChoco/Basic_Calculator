67dcf74053477b7935019798592a89153c784abe
DaChoko <Mailechokoe2@gmail.com>
2024-11-24 15:08:00 +0200

    Quadratic equations now work

---
+++ Quad_functions/quadScript.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const first_term = document.getElementById('a');
+    const second_term = document.getElementById('b');
+    const third_term = document.getElementById('c');
+    const x = document.getElementById('x-num');
+
+    const calculate = document.getElementById('calculate');
+    const show = document.getElementById('out')
+
+    calculate.addEventListener("click", () => {
+        show.value = quadEquation(first_term.value, second_term.value, third_term.value, x.value);
+    } 
+    );
+
+} )
+
+function quadEquation(a, b, c, x){
+
+     return ( a*(x)^2 + b*(x) + c );
+    
+}
