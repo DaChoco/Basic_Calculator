e32abcbd661163a98eb010d51df6924ef02b3a72
DaChoko <Mailechokoe2@gmail.com>
2024-11-29 21:34:21 +0200

    final tweaks to display. Figured out how string functions work

--- Quad_functions/quadScript.js
+++ Quad_functions/quadScript.js
@@ -1,3 +1,10 @@
+window.onload = function(){
+    console.log("We have loaded!");
+
+    let a_val = document.getElementById('a');
+    let b_val = document.getElementById('b');
+    let c_val = document.getElementById('c');
+}
 
 document.addEventListener("DOMContentLoaded", () =>{
     const first_term = document.getElementById('a');
@@ -6,15 +13,19 @@ document.addEventListener("DOMContentLoaded", () =>{
     const x = document.getElementById('x-num');
 
     const calculate = document.getElementById('calculate');
+    const clear = document.getElementById('clear');
     const show = document.getElementById('out')
 
     calculate.addEventListener("click", () => {
         show.value = quadEquation(first_term.value, second_term.value, third_term.value, x.value);
         const displaying = document.getElementById('Quadratic-Showcase');
 
-        displaying.textContent = first_term.value + (x.value) + "^2";
+
+        displaying.textContent = "f(x) =" + first_term.value.toString() + "(x)^2 + " + second_term.value.toString() + "(x) + " + third_term.value.toString();
     } 
     );
+
+    clear.addEventListener("click", clearing);
 
 } )
 
@@ -22,4 +33,19 @@ function quadEquation(a, b, c, x){
 
      return ( a*(x)^2 + b*(x) + c );
     
+}
+
+function clearing(){
+
+    const first_term = document.getElementById('a');
+    const second_term = document.getElementById('b');
+    const third_term = document.getElementById('c');
+    const x = document.getElementById('x-num');
+    const show = document.getElementById('out')
+    
+    first_term.value = 0;
+    second_term.value = 0;
+    third_term.value = 0;
+    x.value = 0;
+
 }
